cba204bbf6d42a4844f11823ea1c2b7d18c398e9
Trevor Keegan <tkgn15@live.com>
2021-05-05 16:18:27 -0500

    updating color so that text is readable

--- src/GUI/ColorScheme.java
+++ src/GUI/ColorScheme.java
@@ -9,7 +9,7 @@ public class ColorScheme {
     public static Color colorTitleYellow = new Color (.9882,.9882,.3294,1);
 
     public static Color colorGameWhite = new Color (.9924, .9137, .9059, 1);
-    public static Color colorGameTeal = new Color(.0157 , .97647, .9882, 1);
+    public static Color colorGameTeal = new Color(34.0/255.0 , 184.0/255.0, 31.0/255.0, 1);
     public static Color colorGameGrey = new Color(.3255,.3176,.3216,1);
     public static Color colorGamePurple = new Color(.9961,.3294,1,1);
 }
